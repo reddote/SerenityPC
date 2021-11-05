49836efb515a284aa89d77b39f972b4f9faa91c3
Muhsin Alper ASAR <alperasar1@gmail.com>
2021-11-05 09:31:29 +0300

    demo asset xray added

---
+++ Assets/XRay Demo Assets/Scripts/XRayReplacement.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+public class XRayReplacement : MonoBehaviour
+{
+    public Shader DroneShader;
+    private Camera camera;
+
+    private void Awake()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    void OnEnable()
+    {
+       
+    }
+
+    private void Update()
+    {
+        
+       
+            
+           camera.SetReplacementShader(DroneShader, "XRay");
+           //camera.clearFlags = CameraClearFlags.Skybox;
+           //camera.clearFlags = CameraClearFlags.Nothing;
+        
+        
+//        if (Input.GetButton("Fire2"))
+//        {
+//            
+//            GetComponent<Camera>().SetReplacementShader(DroneShader, "XRay");
+//            GetComponent<Camera>().clearFlags = CameraClearFlags.Skybox;
+//            GetComponent<Camera>().clearFlags = CameraClearFlags.Nothing;
+//        }
+//        else
+//        {
+//            GetComponent<Camera>().ResetReplacementShader();
+//        }
+    }
+
+}
